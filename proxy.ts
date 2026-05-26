e6526d58cd0a3a20893749ff5939c09e27b8768a
jlnalexander5-sudo <jlnalexander5@gmail.com>
2026-05-27 08:50:57 +1000

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,21 @@
 import { updateSession } from "@/lib/supabase/proxy";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/api/cron")) {
+    return NextResponse.next();
+  }
+
   return await updateSession(request);
 }
-if (request.nextUrl.pathname.startsWith("/api/cron")) {
-  return NextResponse.next();
-}
+
 export const config = {
   matcher: [
     /*
      * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
+     * - _next/static
+     * - _next/image
+     * - favicon.ico
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
